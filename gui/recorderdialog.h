40fe2dd9b6c2edad7737695546bfe43e9028c2a6
jakimushka <jakimushka@gmail.com>
2012-06-18 16:09:39 +0300

    GUI: Add screenshots viewer to recorder dialog

--- gui/recorderdialog.h
+++ gui/recorderdialog.h
@@ -1,5 +1,6 @@
 #ifndef GUI_RECORDER_DIALOG_H
 #define GUI_RECORDER_DIALOG_H
+#include "common/stream.h"
 #include "gui/dialog.h"
 namespace GUI {
 
@@ -13,13 +14,16 @@ class StaticTextWidget;
 
 class RecorderDialog : public GUI::Dialog {
 private:
+	Common::SeekableReadStream *_playbackFile;
 	Common::String _target;
 	Common::String _filename;
+	int _currentScreenshot;
 	GUI::ListWidget *_list;
 	GUI::ContainerWidget *_container;
 	GUI::GraphicsWidget *_gfxWidget;
 	void updateList();
 	void updateSelection(bool redraw);
+	void updateScreenshot();
 	Common::String generateRecordFileName();
 	bool isStringInList(const Common::String &recordName);
 public:
@@ -30,6 +34,8 @@ public:
 	};
 	RecorderDialog();
 	~RecorderDialog();
+	Graphics::Surface *getScreenShot(int number);
+	bool skipToNextScreenshot();
 	virtual void handleCommand(GUI::CommandSender *sender, uint32 cmd, uint32 data);
 	int runModal(Common::String &target);
 	virtual void reflowLayout();
